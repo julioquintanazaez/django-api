961fd6289c449ccf883a73266738290b14117992
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-12-12 23:53:01 -0500

    first commit

---
+++ myproject/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import HelloWorldSerializer
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        data = {"message": "Hello, World!"}
+        serializer = HelloWorldSerializer(data=data)
+        serializer.is_valid()
+        return Response(serializer.data)
