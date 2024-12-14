bb282f59abedb5d5bec7d8a8eba4999a2fda2838
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-12-14 00:27:24 -0500

    dadf

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python -m manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python -m manage.py migrate
+python manage.py migrate
